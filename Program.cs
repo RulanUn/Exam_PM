25ac653954d7f7d36a3956b4fbe44851d4ae8828
Ruslan <52190585+RulanUn@users.noreply.github.com>
2021-02-01 15:54:12 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exam_PM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
