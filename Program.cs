58e56ce77a92ad4f3a12a8a3e78d981f7834bb5f
Ruslan <rus516815@gmail.com>
2021-02-01 17:02:13 +0600

    add new method

--- Program.cs
+++ Program.cs
@@ -4,9 +4,16 @@ namespace Exam_PM
 {
     class Program
     {
+        static public void RandomNumber()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next();
+            Console.WriteLine("Random number is "+randomNumber);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            RandomNumber();
         }
     }
 }
